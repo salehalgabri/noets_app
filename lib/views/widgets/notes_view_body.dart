3e302a3b13b1d116ecb6156dcbfcd279a9dd0f9d
Saleh AL-gabri <s96557867@gmail.com>
2025-01-01 04:49:43 +0300

    Create custom app bar  create notes view body  and create custom search icon

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24,
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.sizeOf(context).width * 0.05,
+          ),
+          const CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
