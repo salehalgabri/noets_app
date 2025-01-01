d3f5ab58909ba64292a2fac1f00b4e046577ff5a
Saleh AL-gabri <s96557867@gmail.com>
2025-01-02 00:23:48 +0300

    Created a custom title text field.

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 36,
+          ),
+          CustomTextField(),
+        ],
+      ),
+    );
   }
 }
