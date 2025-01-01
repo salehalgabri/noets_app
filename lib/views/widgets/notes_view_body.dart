bfc12dd6fb1ecce75433a1845056a6e23859d426
Saleh AL-gabri <s96557867@gmail.com>
2025-01-01 05:31:24 +0300

    Created a custom note item and added the Poppins font family.

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -16,6 +17,7 @@ class NotesViewBody extends StatelessWidget {
             height: MediaQuery.sizeOf(context).width * 0.05,
           ),
           const CustomAppBar(),
+          const NoteItem(),
         ],
       ),
     );
