fea4685f90f4c522c31afca53347888397b75a5f
Saleh AL-gabri <s96557867@gmail.com>
2025-01-04 00:50:10 +0300

    Removed unnecessary import statements.

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
 
@@ -18,7 +17,7 @@ class AddNoteBottomSheet extends StatelessWidget {
         child: BlocConsumer<AddNoteCubit, AddNoteState>(
           listener: (context, state) {
             if (state is AddNoteFailure) {
-              print('failed ${state.errorMessage} ');
+              debugPrint('failed ${state.errorMessage} ');
             }
             if (state is AddNoteSuccess) {
               Navigator.pop(context);
