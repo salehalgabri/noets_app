e47e7ebb44e13125f4a5a61e3bd0c77079eb8c96
Saleh AL-gabri <s96557867@gmail.com>
2025-01-03 18:35:19 +0300

    Added input handling, data validation, and keyboard padding.

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -2,24 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key, this.onTap});
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.sizeOf(context).width,
-      height: 55,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: kPrimaryColor,
-      ),
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontFamily: 'Poppins-Regular',
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.sizeOf(context).width,
+        height: 55,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: kPrimaryColor,
+        ),
+        child: const Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontFamily: 'Poppins-Regular',
+            ),
           ),
         ),
       ),
