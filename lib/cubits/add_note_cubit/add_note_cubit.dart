6efe423ef6f80cb552587cbae421995ffdaa97db
Saleh AL-gabri <s96557867@gmail.com>
2025-01-03 23:08:25 +0300

    Added addNote method to AddNoteCubit.

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
@@ -7,7 +9,14 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-  addNote(NoteModel noteModel){
-    
+  addNote(NoteModel noteModel) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box(kNotesBox);
+      emit(AddNoteSuccess());
+      await notesBox.add(noteModel);
+    } catch (e) {
+      AddNoteFailure(errorMessage: e.toString());
+    }
   }
 }
