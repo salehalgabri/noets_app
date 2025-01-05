33503b68013ffc5fe4e271c056997e8d1d429ea2
Saleh AL-gabri <s96557867@gmail.com>
2025-01-05 22:10:38 +0300

    Implemented part one of Colors ListView.

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/views/widgets/colors_list_view.dart';
 import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
@@ -46,6 +47,10 @@ class _AddNoteFormState extends State<AddNoteForm> {
             },
           ),
           const SizedBox(
+            height: 16,
+          ),
+        const  ColorsListView(),
+          const SizedBox(
             height: 32,
           ),
           BlocBuilder<AddNoteCubit, AddNoteState>(
@@ -85,3 +90,5 @@ class _AddNoteFormState extends State<AddNoteForm> {
     );
   }
 }
+
+
